5696490e635042ba4165cae35e54177e01b0e316
枫魂777 <zhangyyxinlang@sina.com>
2018-03-09 08:01:10 +0800

    Initial Commit

---
+++ zylOC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  zylOC
+//
+//  Created by yy z on 2018/3/9.
+//  Copyright © 2018年 zly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ zylOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zylOC
+//
+//  Created by yy z on 2018/3/9.
+//  Copyright © 2018年 zly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
